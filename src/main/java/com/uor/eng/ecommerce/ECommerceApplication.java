61305dc8ba334c5ab5a1a9de6bffcb5b0eb097f9
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-12-14 16:00:20 +0530

    Updated

---
+++ src/main/java/com/uor/eng/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.uor.eng.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ECommerceApplication.class, args);
+  }
+
+}
