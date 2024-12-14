c895b677b4836da75c0bbc16c7f0eb294baedcb2
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-12-14 16:06:06 +0530

    Updated With JWT and Spring Security

--- src/main/java/com/ecommerce/project/ECommerceApplication.java
+++ src/main/java/com/ecommerce/project/ECommerceApplication.java
@@ -1,4 +1,4 @@
-package com.uor.eng.ecommerce;
+package com.ecommerce.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
